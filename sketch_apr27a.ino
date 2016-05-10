81b27659373a0018fb68aaf0a30aedd80511c72d
TerryBluemix <terrybluemix@gmail.com>
2016-05-11 00:54:47 +0800

    Update sketch_apr27a.ino

--- sketch_apr27a.ino
+++ sketch_apr27a.ino
@@ -19,7 +19,7 @@ void setup() {
 void loop() {
  //送出測試資料 lat: 25.045361, long:121.522544, batteryLevel:200, temp: 30, state: 10(00001010)
   LoRa.print("AT+DTX=22,073E4970017E2971C81E0A\r");
-  delay(500);
+  delay(1500*60);
  
   // 若收到藍牙模組的資料，則送到「序列埠監控視窗」
   ;
