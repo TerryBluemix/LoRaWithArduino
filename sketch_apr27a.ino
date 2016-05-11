3b70657c8a34733a7b258f77ed6ad04c220e2956
TerryBluemix <terrybluemix@gmail.com>
2016-05-11 12:29:37 +0800

    Update sketch_apr27a.ino

--- sketch_apr27a.ino
+++ sketch_apr27a.ino
@@ -20,7 +20,12 @@ void loop() {
  //送出測試資料 lat: 25.045361, long:121.522544, batteryLevel:200, temp: 30, state: 10(00001010)
   LoRa.print("AT+DTX=22,073E4970017E2971C81E0A\r");
   delay(1500*60);
- 
+  //自行輸入AT command
+ // if (Serial.available()) {
+ //    val = Serial.read();
+ //    LoRa.print(val);
+ //    //LoRa.print("AT+DTX=3,\"123\"");
+ //  }
   // 若收到藍牙模組的資料，則送到「序列埠監控視窗」
   ;
   if (LoRa.available() ) {
