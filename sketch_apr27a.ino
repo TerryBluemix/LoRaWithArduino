1d5827adac2959f6d59217eae33c3cf1a212446b
TerryBluemix <terrybluemix@gmail.com>
2016-05-11 13:04:17 +0800

    Update sketch_apr27a.ino

--- sketch_apr27a.ino
+++ sketch_apr27a.ino
@@ -1,17 +1,16 @@
 #include <SoftwareSerial.h>   // 引用程式庫
  
-// 定義連接藍牙模組的序列埠
+// 定義連接LoRa模組的序列埠
 SoftwareSerial LoRa(10, 11); // 接收腳, 傳送腳
 char val;  // 儲存接收資料的變數
  
 void setup() {
   Serial.begin(9600);   // 與電腦序列埠連線
   Serial.println("LoRa is ready!");
-  //Serial.write("AT+DTX=3,'123'");
   Serial.write('\r');
   // 設定LoRa模組的連線速率
   LoRa.begin(9600);
-  //LoRa.print("AT+DTX=3,'123'\r");
+  
   //用print 的方式自動下達AT command
   
 }
@@ -24,9 +23,9 @@ void loop() {
  if (Serial.available()) {
     val = Serial.read();
     LoRa.print(val);
-    //LoRa.print("AT+DTX=3,\"123\"");
+    
   }
-  // 若收到藍牙模組的資料，則送到「序列埠監控視窗」
+  // 若收到LoRa模組的資料，則送到「序列埠監控視窗」
   ;
   if (LoRa.available() ) {
     val = LoRa.read();
